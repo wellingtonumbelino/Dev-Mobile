566c92ff69a79ddb397c5269fdaa30764040db17
wellingtonumbelino <wellingtonsi@alu.ufc.br>
2021-01-09 15:46:10 -0300

    primeiro commit

---
+++ java/br/quixada/ufc/dadm/MainActivity.java
@@ -0,0 +1,112 @@
+package br.quixada.ufc.dadm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+
+    Spinner spnr;
+
+    String[] celebrities = {
+            "Chris Hemsworth",
+            "Jennifer Lawrence",
+            "Jessica Alba",
+            "Brad Pitt",
+            "Tom Cruise",
+            "Johnny Depp",
+            "Megan Fox",
+            "Paul Walker",
+            "Vin Diesel"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String str[]={"Arun","Mathev","Vishnu","Vishal","Arjun",
+                "Arul","Balaji","Babu","Boopathy","Godwin","Nagaraj"};
+
+        AutoCompleteTextView t1 = (AutoCompleteTextView)
+                findViewById(R.id.autoCompleteTextView1);
+
+        ArrayAdapter<String> adp=new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line,str);
+
+        t1.setThreshold(1);
+        t1.setAdapter(adp);
+
+        spnr = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, celebrities);
+
+        spnr.setAdapter(adapter);
+        spnr.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+
+                    @Override
+                    public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                               int arg2, long arg3) {
+
+                        int position = spnr.getSelectedItemPosition();
+                        Toast.makeText(getApplicationContext(),"You have selected "+celebrities[+position],Toast.LENGTH_LONG).show();
+                        // TODO Auto-generated method stub
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> arg0) {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                }
+        );
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(MainActivity.this, button1);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater()
+                        .inflate(R.menu.popup_menu, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(
+                                MainActivity.this,
+                                "You Clicked : " + item.getTitle(),
+                                Toast.LENGTH_SHORT
+                        ).show();
+                        return true;
+                    }
+                });
+
+                popup.show(); //showing popup menu
+            }
+        }); //closing the setOnClickListener method
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
